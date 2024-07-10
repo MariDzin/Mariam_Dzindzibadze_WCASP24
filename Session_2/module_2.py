37669e2c951d9d58e3d4bc2e0d852f029c00bf81
mari_dzin <mariamdzindzibadze2002@gmail.com>
2024-07-10 20:32:05 +0400

    updated

--- Session_2/module_2.py
+++ Session_2/module_2.py
@@ -1,4 +1,3 @@
-
 from typing import Any, Dict, List, Tuple
 
 
@@ -14,11 +13,14 @@ def task_1(data_1: Dict[str, int], data_2: Dict[str, int]):
     return combined_dict
 
 
+# first I created dictionary1 copy then with for loop if key is in the dictionary then value will be
+# increased, if not in the dictionary I  add key and value
+
 def task_2():
     result = {i: i ** 2 for i in range(1, 16)}
     return result
 
-
+# returning result where for each i(key) value will be i in square
 def task_3(data: Dict[Any, List[str]]):
     def generate_combinations(key, current_combination):
         if not key:
@@ -34,6 +36,7 @@ def task_3(data: Dict[Any, List[str]]):
     generate_combinations(keys, "")
     return combinations
 
+# here  function considers all possible combinations of letters associated with each key.
 
 def task_4(data: Dict[str, int]):
     if not data:
@@ -45,6 +48,9 @@ def task_4(data: Dict[str, int]):
     return highest_keys
 
 
+# in this task I sorted data and  from sorted i chose first 3 or if nothing in the data then empty list
+#will be returned
+
 def task_5(data: List[Tuple[Any, Any]]) -> Dict[str, List[int]]:
     result = {}
     for key, value in data:
@@ -55,14 +61,23 @@ def task_5(data: List[Tuple[Any, Any]]) -> Dict[str, List[int]]:
     return result
 
 
+# here I created dictionary result, then go thought key value, and if key is in result then values will
+# appended for the same key and if it is  not seen in the dictionary then I just add key and value in the dictionary
+
+
 def task_6(data: List[Any]):
-    diction = {}
+    seen = set()
     result = []
-    for i in data:
-        if i not in diction:
-            diction[i] = 7  # random value does not matter
-            result.append(i)
+    for item in data:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
     return result
+
+
+#If an element is not in seen, it means it's the first time it appears
+# and we add it to both the seen set and the result list.
+# This way each  element appears only once in the result list. as set it not saving duplicates
 
 
 def task_7(words: [List[str]]) -> str:
@@ -84,6 +99,14 @@ def task_8(haystack: str, needle: str) -> int:
     if needle == "":
         return 0
 
-    index = haystack.find(needle)
+    len_haystack = len(haystack)
+    len_needle = len(needle)
 
-    return index
+    for i in range(len_haystack - len_needle + 1):
+        if haystack[i:i + len_needle] == needle:
+            return i
+
+    return -1
+
+# here the idea is go thought  the haystack string and check substrings of length equal to the needle.
+# If we find a match, then returning the starting index of the match and  If no match, return -1.

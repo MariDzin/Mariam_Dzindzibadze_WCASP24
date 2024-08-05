8bbd370033e6caa3902b768a9ead74ef55c191d9
mari_dzin <mariamdzindzibadze2002@gmail.com>
2024-08-05 20:56:11 +0400

    updated task6

--- DWH/Task6/extraction.sql
+++ DWH/Task6/extraction.sql
@@ -1,18 +1,17 @@
-
 -- Creating DEALSIZE_ID sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.dealsize_id_seq;
 
 -- Combining data from both sources
 WITH combined_dealsizes AS (
     SELECT DISTINCT
-        COALESCE(us.dealsize,'n. a.' ) AS DEALSIZE,
+        us.dealsize AS DEALSIZE,
         us.dealsize AS DEALSIZE_SRC_ID,
         'sa_us_orders' AS SOURCE_SYSTEM,
         'src_us_order' AS SOURCE_ENTITY
     FROM sa_us_orders.src_us_order us
     UNION ALL
     SELECT DISTINCT
-        COALESCE(nous.deal_size, 'n. a.') AS DEALSIZE,
+        nous.deal_size AS DEALSIZE,
         nous.deal_size AS DEALSIZE_SRC_ID,
         'sa_no_usa_orders' AS SOURCE_SYSTEM,
         'src_no_usa_order' AS SOURCE_ENTITY
@@ -37,22 +36,21 @@ COMMIT;
 SELECT * FROM BL_3NF.CE_DEALSIZES;
 
 
---  PAYMENT_METHOD_ID sequence 
+-- Creating PAYMENT_METHOD_ID sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.payment_method_id_seq;
 
-
--- Combine data from both sources
+-- Combining data from both sources
 WITH combined_payment_methods AS (
     SELECT DISTINCT
-        COALESCE(us.payment_method, 'n. a.') AS PAYMENT_METHOD,
-        COALESCE(us.payment_method, 'n. a.') AS PAYMENT_SRC_ID,  
+        us.payment_method AS PAYMENT_METHOD,
+        us.payment_method AS PAYMENT_SRC_ID,
         'sa_us_orders' AS SOURCE_SYSTEM,
         'src_us_order' AS SOURCE_ENTITY
     FROM sa_us_orders.src_us_order us
     UNION ALL
     SELECT DISTINCT
-        COALESCE(nous.payment_method, 'n. a.') AS PAYMENT_METHOD,
-        COALESCE(nous.payment_method, 'n. a.') AS PAYMENT_SRC_ID,  
+        nous.payment_method AS PAYMENT_METHOD,
+        nous.payment_method AS PAYMENT_SRC_ID,
         'sa_no_usa_orders' AS SOURCE_SYSTEM,
         'src_no_usa_order' AS SOURCE_ENTITY
     FROM sa_no_usa_orders.src_no_usa_order nous
@@ -76,18 +74,17 @@ COMMIT;
 SELECT * FROM BL_3NF.CE_PAYMENT_METHODS;
 
 
--- okay now lets fill other tables, next is product 
-
+-- Creating PRODUCT_ID sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.product_id_seq;
 
--- combining both source data
+-- Combining data from both sources
 WITH combined_products AS (
     SELECT DISTINCT
         us.productcode AS PRODUCTCODE,
         us.productline AS PRODUCTLINE,
         us.priceeach::VARCHAR AS PRICEEACH,
         us.msrp::VARCHAR AS MSRP,
-        COALESCE(us.productcode, 'n. a.') AS PRODUCT_SRC_ID,  
+        us.productcode AS PRODUCT_SRC_ID,
         'sa_us_orders' AS SOURCE_SYSTEM,
         'src_us_order' AS SOURCE_ENTITY
     FROM sa_us_orders.src_us_order us
@@ -97,7 +94,7 @@ WITH combined_products AS (
         nous.product_line AS PRODUCTLINE,
         nous.price_for_each AS PRICEEACH,
         nous.ms_rp AS MSRP,
-        COALESCE(nous.product_model, 'n. a.') AS PRODUCT_SRC_ID,  
+        nous.product_model AS PRODUCT_SRC_ID,
         'sa_no_usa_orders' AS SOURCE_SYSTEM,
         'src_no_usa_order' AS SOURCE_ENTITY
     FROM sa_no_usa_orders.src_no_usa_order nous
@@ -121,26 +118,23 @@ ON CONFLICT (PRODUCTCODE) DO NOTHING;
 
 COMMIT;
 
+SELECT * FROM BL_3NF.CE_PRODUCTS;
 
 
-select * from bl_3nf.ce_products cp 
-
-
-
--- COUNTRY_ID sequence 
+-- Creating COUNTRY_ID sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.country_id_seq;
 
--- combining data from both sources
+-- Combining data from both sources
 WITH combined_countries AS (
     SELECT DISTINCT
-        us.country AS COUNTRY_NAME,
+        COALESCE(us.country, 'n. a.') AS COUNTRY_NAME,
         COALESCE(us.country, 'n. a.') AS COUNTRY_SRC_ID,
         'sa_us_orders' AS SOURCE_SYSTEM,
         'src_us_order' AS SOURCE_ENTITY
     FROM sa_us_orders.src_us_order us
     UNION ALL
     SELECT DISTINCT
-        nous.country_name AS COUNTRY_NAME,
+        COALESCE(nous.country_name, 'n. a.') AS COUNTRY_NAME,
         COALESCE(nous.country_name, 'n. a.') AS COUNTRY_SRC_ID,
         'sa_no_usa_orders' AS SOURCE_SYSTEM,
         'src_no_usa_order' AS SOURCE_ENTITY
@@ -165,24 +159,22 @@ COMMIT;
 SELECT * FROM BL_3NF.CE_COUNTRIES;
 
 
-
---  COUNTRY_ID sequence
+-- Creating STATE_ID sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.state_id_seq;
 
--- combining data from both sources
-
+-- Combining data from only one source
 WITH combined_states AS (
     SELECT DISTINCT
         us.state AS STATE_NAME,
-        COALESCE(us.state, 'n. a.') AS STATE_SRC_ID,  
-        c.country_id AS COUNTRY_ID,
+        COALESCE(us.state, 'n. a.') AS STATE_SRC_ID,
+        COALESCE(c.country_id, -1) AS COUNTRY_ID,
         'sa_us_orders' AS SOURCE_SYSTEM,
         'src_us_order' AS SOURCE_ENTITY
     FROM sa_us_orders.src_us_order us
-    LEFT JOIN BL_3NF.CE_COUNTRIES c 
-    ON c.source_system = 'sa_us_orders'
+    LEFT JOIN BL_3NF.CE_COUNTRIES c
+    ON us.country = c.country_src_id
+    AND c.source_system = 'sa_us_orders'
     AND c.source_entity = 'src_us_order'
-    AND us.country = c.country_src_id
 )
 INSERT INTO BL_3NF.CE_STATES (
     STATE_ID, STATE_NAME, STATE_SRC_ID, COUNTRY_ID, TA_INSERT_DT, TA_UPDATE_DT, SOURCE_SYSTEM, SOURCE_ENTITY
@@ -201,14 +193,13 @@ ON CONFLICT (STATE_NAME) DO NOTHING;
 
 COMMIT;
 
--- Verify the inserted data
 SELECT * FROM BL_3NF.CE_STATES;
 
 
--- next is cities
---  COUNTRY_ID sequence creating 
+-- Creating CITY_ID sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.city_id_seq;
--- combine data from both sources
+
+-- Combining data from both sources
 WITH us_cities AS (
     SELECT DISTINCT
         us.city AS CITY_NAME,
@@ -217,13 +208,16 @@ WITH us_cities AS (
         'sa_us_orders' AS SOURCE_SYSTEM,
         'src_us_order' AS SOURCE_ENTITY
     FROM sa_us_orders.src_us_order us
-    LEFT JOIN BL_3NF.CE_STATES s ON us.state = s.state_name
+    LEFT JOIN BL_3NF.CE_STATES s 
+    ON us.state = s.state_name
+    AND s.source_system = 'sa_us_orders'
+    AND s.source_entity = 'src_us_order'
 ),
 non_us_cities AS (
     SELECT DISTINCT
         nous.city_name AS CITY_NAME,
         COALESCE(nous.city_name, 'n. a.') AS CITY_SRC_ID,
-        -1 AS STATE_ID,  -- default value for state_id
+        -1 AS STATE_ID,
         'sa_no_usa_orders' AS SOURCE_SYSTEM,
         'src_no_usa_order' AS SOURCE_ENTITY
     FROM sa_no_usa_orders.src_no_usa_order nous
@@ -249,9 +243,13 @@ ON CONFLICT (CITY_NAME) DO NOTHING;
 
 COMMIT;
 
-select * from bl_3nf.ce_cities cc 
+SELECT * FROM BL_3NF.CE_CITIES;
 
---combining 
+
+-- Creating ADDRESS_ID sequence
+CREATE SEQUENCE IF NOT EXISTS BL_3NF.address_id_seq;
+
+-- Combining data from both sources
 WITH combined_addresses AS (
     SELECT DISTINCT
         COALESCE(us.addressline1, 'n. a.') AS ADDRESSLINE1,
@@ -262,6 +260,8 @@ WITH combined_addresses AS (
     FROM sa_us_orders.src_us_order us
     LEFT JOIN BL_3NF.CE_CITIES c 
     ON us.city = c.city_name
+    AND c.source_system = 'sa_us_orders'
+    AND c.source_entity = 'src_us_order'
     UNION ALL
     SELECT DISTINCT
         COALESCE(nous.address_line, 'n. a.') AS ADDRESSLINE1,
@@ -272,6 +272,8 @@ WITH combined_addresses AS (
     FROM sa_no_usa_orders.src_no_usa_order nous
     LEFT JOIN BL_3NF.CE_CITIES c 
     ON nous.city_name = c.city_name
+    AND c.source_system = 'sa_no_usa_orders'
+    AND c.source_entity = 'src_no_usa_order'
 )
 INSERT INTO BL_3NF.CE_ADDRESSES (
     ADDRESS_ID, ADDRESSLINE1, ADDRESS_SRC_ID, CITY_ID, TA_INSERT_DT, TA_UPDATE_DT, SOURCE_SYSTEM, SOURCE_ENTITY
@@ -290,18 +292,16 @@ ON CONFLICT (ADDRESSLINE1) DO NOTHING;
 
 COMMIT;
 
-
-select * from bl_3nf.ce_addresses ca 
-
+SELECT * FROM BL_3NF.CE_ADDRESSES;
 
 
--- Create the sequence for CUSTOMER_ID
+-- Creating CUSTOMER_ID sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.customer_id_seq;
 
--- Combine data from both sources
+-- Combining data from both sources
 WITH combined_customers AS (
     SELECT DISTINCT
-        COALESCE(us.customer_id::BIGINT, -1) AS CUSTOMER_ID,  
+        COALESCE(us.customer_id::BIGINT, -1) AS CUSTOMER_ID,
         COALESCE(us.customername, 'n. a.') AS CUSTOMERNAME,
         COALESCE(us.contactfirstname, 'n. a.') AS CONTACTFIRSTNAME,
         COALESCE(us.contactlastname, 'n. a.') AS CONTACTLASTNAME,
@@ -317,7 +317,7 @@ WITH combined_customers AS (
     AND a.source_entity = 'src_us_order'
     UNION ALL
     SELECT DISTINCT
-        COALESCE(nous.customers_id::BIGINT, -1) AS CUSTOMER_ID, 
+        COALESCE(nous.customers_id::BIGINT, -1) AS CUSTOMER_ID,
         COALESCE(nous.cust_name, 'n. a.') AS CUSTOMERNAME,
         COALESCE(nous.cust_firstname, 'n. a.') AS CONTACTFIRSTNAME,
         COALESCE(nous.cust_lastname, 'n. a.') AS CONTACTLASTNAME,
@@ -333,8 +333,8 @@ WITH combined_customers AS (
     AND a.source_entity = 'src_no_usa_order'
 )
 INSERT INTO BL_3NF.CE_CUSTOMERS_SCD (
-    CUSTOMER_ID, CUSTOMERNAME, CONTACTFIRSTNAME, CONTACTLASTNAME, PHONE, CUSTOMER_SRC_ID, ADDRESS_ID, 
-    START_DT, END_DT, IS_ACTIVE, TA_INSERT_DT,  SOURCE_SYSTEM, SOURCE_ENTITY
+    CUSTOMER_ID, CUSTOMERNAME, CONTACTFIRSTNAME, CONTACTLASTNAME, PHONE, CUSTOMER_SRC_ID, ADDRESS_ID,
+    START_DT, END_DT, IS_ACTIVE, TA_INSERT_DT, SOURCE_SYSTEM, SOURCE_ENTITY
 )
 SELECT
     nextval('BL_3NF.customer_id_seq') AS CUSTOMER_ID,
@@ -351,62 +351,80 @@ SELECT
     cd.SOURCE_SYSTEM,
     cd.SOURCE_ENTITY
 FROM combined_customers cd
-ON CONFLICT (CUSTOMERNAME)
-DO UPDATE SET
-    CONTACTFIRSTNAME = EXCLUDED.CONTACTFIRSTNAME,
-    CONTACTLASTNAME = EXCLUDED.CONTACTLASTNAME,
-    PHONE = EXCLUDED.PHONE,
-    CUSTOMER_SRC_ID = EXCLUDED.CUSTOMER_SRC_ID,
-    ADDRESS_ID = EXCLUDED.ADDRESS_ID
+ON CONFLICT (CUSTOMERNAME) DO NOTHING;
 
 COMMIT;
 
-select * from bl_3nf.ce_customers_scd ccs 
+SELECT * FROM BL_3NF.CE_CUSTOMERS_SCD;
 
 
--- Create the sequence for ORDERNUMBER
+-- Creating ORDERNUMBER sequence
 CREATE SEQUENCE IF NOT EXISTS BL_3NF.ordernumber_seq;
 
--- Combine data from both sources
+-- Combining data from both sources
 WITH combined_orders AS (
     SELECT DISTINCT
-        COALESCE(us.ordernumber::BIGINT, -1) AS ORDERNUMBER,  
-        COALESCE(us.quantityordered::INTEGER, -1) AS QUANTITYORDERED,  
-        COALESCE(us.sales::NUMERIC, -1) AS SALES,  
+        COALESCE(us.ordernumber::BIGINT, -1) AS ORDERNUMBER,
+        COALESCE(us.quantityordered::INTEGER, -1) AS QUANTITYORDERED,
+        COALESCE(us.sales::NUMERIC, -1) AS SALES,
         COALESCE(pm.PAYMENT_METHOD_ID, -1) AS PAYMENT_METHOD_ID,
         COALESCE(ds.DEALSIZE_ID, -1) AS DEALSIZE_ID,
         COALESCE(p.PRODUCT_ID, -1) AS PRODUCT_ID,
         COALESCE(c.CUSTOMER_ID, -1) AS CUSTOMER_ID,
-        COALESCE(us.orderdate::DATE, '1900-01-01') AS EVENT_DT,  
+        COALESCE(us.orderdate::DATE, '1900-01-01') AS EVENT_DT,
         'sa_us_orders' AS SOURCE_SYSTEM,
         'src_us_order' AS SOURCE_ENTITY,
-        COALESCE(us.ordernumber::VARCHAR, 'n. a.') AS ORDER_SRC_ID  
+        COALESCE(us.ordernumber::VARCHAR, 'n. a.') AS ORDER_SRC_ID
     FROM sa_us_orders.src_us_order us
-    LEFT JOIN BL_3NF.CE_PAYMENT_METHODS pm ON us.payment_method = pm.PAYMENT_METHOD
-    LEFT JOIN BL_3NF.CE_DEALSIZES ds ON us.dealsize = ds.DEALSIZE
-    LEFT JOIN BL_3NF.CE_PRODUCTS p ON us.productcode = p.PRODUCTCODE
-    LEFT JOIN BL_3NF.CE_CUSTOMERS_SCD c ON us.customer_id::BIGINT = c.CUSTOMER_ID  
+    LEFT JOIN BL_3NF.CE_PAYMENT_METHODS pm 
+    ON us.payment_method = pm.PAYMENT_METHOD
+    AND pm.source_system = 'sa_us_orders'
+    AND pm.source_entity = 'src_us_order'
+    LEFT JOIN BL_3NF.CE_DEALSIZES ds 
+    ON us.dealsize = ds.DEALSIZE
+    AND ds.source_system = 'sa_us_orders'
+    AND ds.source_entity = 'src_us_order'
+    LEFT JOIN BL_3NF.CE_PRODUCTS p 
+    ON us.productcode = p.PRODUCTCODE
+    AND p.source_system = 'sa_us_orders'
+    AND p.source_entity = 'src_us_order'
+    LEFT JOIN BL_3NF.CE_CUSTOMERS_SCD c 
+    ON us.customer_id::BIGINT = c.CUSTOMER_ID
+    AND c.source_system = 'sa_us_orders'
+    AND c.source_entity = 'src_us_order'
     UNION ALL
     SELECT DISTINCT
-        COALESCE(nous.order_id::BIGINT, -1) AS ORDERNUMBER,  
-        COALESCE(nous.quantity::INTEGER, -1) AS QUANTITYORDERED,  
-        COALESCE(nous.sales_amount::NUMERIC, -1) AS SALES,  
+        COALESCE(nous.order_id::BIGINT, -1) AS ORDERNUMBER,
+        COALESCE(nous.quantity::INTEGER, -1) AS QUANTITYORDERED,
+        COALESCE(nous.sales_amount::NUMERIC, -1) AS SALES,
         COALESCE(pm.PAYMENT_METHOD_ID, -1) AS PAYMENT_METHOD_ID,
         COALESCE(ds.DEALSIZE_ID, -1) AS DEALSIZE_ID,
         COALESCE(p.PRODUCT_ID, -1) AS PRODUCT_ID,
         COALESCE(c.CUSTOMER_ID, -1) AS CUSTOMER_ID,
-        COALESCE(nous.date_of_order::DATE, '1900-01-01') AS EVENT_DT, 
+        COALESCE(nous.date_of_order::DATE, '1900-01-01') AS EVENT_DT,
         'sa_no_usa_orders' AS SOURCE_SYSTEM,
         'src_no_usa_order' AS SOURCE_ENTITY,
-        COALESCE(nous.order_id::VARCHAR, 'n. a.') AS ORDER_SRC_ID  
+        COALESCE(nous.order_id::VARCHAR, 'n. a.') AS ORDER_SRC_ID
     FROM sa_no_usa_orders.src_no_usa_order nous
-    LEFT JOIN BL_3NF.CE_PAYMENT_METHODS pm ON nous.payment_method = pm.PAYMENT_METHOD
-    LEFT JOIN BL_3NF.CE_DEALSIZES ds ON nous.deal_size = ds.DEALSIZE
-    LEFT JOIN BL_3NF.CE_PRODUCTS p ON nous.product_model = p.PRODUCTCODE
-    LEFT JOIN BL_3NF.CE_CUSTOMERS_SCD c ON nous.customers_id::BIGINT = c.CUSTOMER_ID  
+    LEFT JOIN BL_3NF.CE_PAYMENT_METHODS pm 
+    ON nous.payment_method = pm.PAYMENT_METHOD
+    AND pm.source_system = 'sa_no_usa_orders'
+    AND pm.source_entity = 'src_no_usa_order'
+    LEFT JOIN BL_3NF.CE_DEALSIZES ds 
+    ON nous.deal_size = ds.DEALSIZE
+    AND ds.source_system = 'sa_no_usa_orders'
+    AND ds.source_entity = 'src_no_usa_order'
+    LEFT JOIN BL_3NF.CE_PRODUCTS p 
+    ON nous.product_model = p.PRODUCTCODE
+    AND p.source_system = 'sa_no_usa_orders'
+    AND p.source_entity = 'src_no_usa_order'
+    LEFT JOIN BL_3NF.CE_CUSTOMERS_SCD c 
+    ON nous.customers_id::BIGINT = c.CUSTOMER_ID
+    AND c.source_system = 'sa_no_usa_orders'
+    AND c.source_entity = 'src_no_usa_order'
 )
 INSERT INTO BL_3NF.CE_ORDERS (
-    ORDERNUMBER, QUANTITYORDERED, SALES, PAYMENT_METHOD_ID, DEALSIZE_ID, PRODUCT_ID, CUSTOMER_ID, 
+    ORDERNUMBER, QUANTITYORDERED, SALES, PAYMENT_METHOD_ID, DEALSIZE_ID, PRODUCT_ID, CUSTOMER_ID,
     EVENT_DT, TA_INSERT_DT, TA_UPDATE_DT, ORDER_SRC_ID, SOURCE_SYSTEM, SOURCE_ENTITY
 )
 SELECT
@@ -429,4 +447,3 @@ ON CONFLICT (ORDERNUMBER) DO NOTHING;
 COMMIT;
 
 SELECT * FROM BL_3NF.CE_ORDERS;
-
